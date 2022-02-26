9403b3a4de2a9bb4fca5f2e0306f0b58eb935623
allen <allenshaw19@gmail.com>
2022-02-27 01:55:51 +0800

    start add meta

---
+++ raft/raft_meta.go
@@ -0,0 +1,43 @@
+package raft
+
+import (
+	"github.com/cockroachdb/pebble"
+	"sync"
+)
+
+// MixedMetaStorage start
+type MixedMetaStorage struct {
+}
+
+// FileBasedSingleMetaStorage start
+type FileBasedSingleMetaStorage struct {
+}
+
+// KVBasedMergedMetaStorage start
+type KVBasedMergedMetaStorage struct {
+}
+
+// WriteTask kvBasedMergedMetaStorageImpl start
+type WriteTask struct {
+	Term     int64
+	VotedFor PeerId
+	VgID     VersionedGroupId
+	Done     Closure
+}
+
+type KVBasedMergedMetaStorageImpl struct {
+	mutex    sync.Mutex
+	isInited bool
+	path     string
+	db       *pebble.DB
+}
+
+func NewKVBasedMergedMetaStorageImpl(path string) *KVBasedMergedMetaStorageImpl {
+	return &KVBasedMergedMetaStorageImpl{
+		path:     path,
+		isInited: false,
+	}
+}
+func (s *KVBasedMergedMetaStorageImpl) Init() error {
+
+}
